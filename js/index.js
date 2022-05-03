04476217002ed1d0b2df9a508f6b8d9a48ba36a9
Josep F.T <josep.faneca@geografos.org>
2022-05-03 10:06:38 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    var infoToast = document.getElementById('infoToast')
+    var toast = new bootstrap.Toast(infoToast)
+    toast.show()
+});
+
+$('#vertical-carousel').bind('mousewheel DOMMouseScroll', function (e) {
+    if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
+        $(this).carousel('prev');
+    }
+    else {
+        $(this).carousel('next');
+
+    }
+});
+
+$("#vertical-carousel").on("touchstart", function (event) {
+    var yTouchPointStart = event.originalEvent.touches[0].pageY;
+    $(this).on("touchmove", function (event) {
+        var yTouchPointEnd = event.originalEvent.touches[0].pageY;
+        if (Math.floor(yTouchPointStart - yTouchPointEnd) > 1) {
+            $(".carousel").carousel('next');
+        }
+        else if (Math.floor(yTouchPointStart - yTouchPointEnd) < -1) {
+            $(".carousel").carousel('prev');
+        }
+    });
+    $(".carousel").on("touchend", function () {
+        $(this).off("touchmove");
+    });
+});
